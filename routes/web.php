f756f5792cf572d7ab144816cbdd44722577c0eb
Alex <01alexomwangi@gmail.com>
2025-12-30 19:54:06 +0300

    added middleware

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
@@ -7,19 +8,25 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-//Route::get('line/', [TaskController::class, 'index']);
+require __DIR__.'/auth.php';
+Route::prefix('')->middleware('auth')->group(function () {
+    Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
+    Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
+    Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
 
+    // Dynamic routes (must be last!)
+    Route::get('/tasks/{id}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
+    Route::put('/tasks/{id}', [TaskController::class, 'update'])->name('tasks.update');
+    Route::delete('/tasks/{id}', [TaskController::class, 'delete'])->name('tasks.delete');
+});
 
-Route::get('/tasks', [TaskController::class, 'index']);
-
-Route::get('/tasks/create', [TaskController::class, 'create']);
-
-Route::post('/tasks', [TaskController::class, 'store']);
-
-Route::get('/tasks/{id}/edit', [TaskController::class, 'edit']);
-
-Route::put('/tasks/{id}', [TaskController::class, 'update']);
-
-Route::delete('/tasks/{id}', [TaskController::class, 'delete']);
